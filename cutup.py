f6c72edee53c6ffadd439a6a9b80460c7455cf43
Ross Goodwin <ross.goodwin@gmail.com>
2014-12-15 16:58:51 -0500

    tabs to spaces

--- cutup.py
+++ cutup.py
@@ -20,41 +20,41 @@ lines = [] # 2D list
 charCount = 0
 
 for t in tokens:
-	charCount += len(t)+1
-	if charCount <= lineLength+1:
-		curLine.append(t)
-	else:
-		lines.append(curLine)
-		curLine = []
-		charCount = len(t)+1
-		curLine.append(t)
+    charCount += len(t)+1
+    if charCount <= lineLength+1:
+        curLine.append(t)
+    else:
+        lines.append(curLine)
+        curLine = []
+        charCount = len(t)+1
+        curLine.append(t)
 
 # Make horizontal cut
 hCutPt = len(lines)/2
 hSections = [lines[:hCutPt], lines[hCutPt:]]
 
 # Make vertical cut
-sections = [[] for i in range(4)]
+sections = [[] for i in range(4)] # 3D list
 for i in range(4):
-	for l in hSections[i/2]:
-		sections[i].append([])
+    for l in hSections[i/2]:
+        sections[i].append([])
 
 for i in range(2):
-	lineCount = 0
-	for l in hSections[i]:
-		charCount = 0
-		for w in l:
-			charCount += len(w)+1
-			if charCount <= lineLength/2 + 1:
-				sections[0+i*2][lineCount].append(w)
-			else:
-				sections[1+i*2][lineCount].append(w)
-		lineCount+=1
+    lineCount = 0
+    for l in hSections[i]:
+        charCount = 0
+        for w in l:
+            charCount += len(w)+1
+            if charCount <= lineLength/2 + 1:
+                sections[0+i*2][lineCount].append(w)
+            else:
+                sections[1+i*2][lineCount].append(w)
+        lineCount+=1
 
 # Rearrange per Burroughs' instructions
 sections.reverse()
 
 # Print result with line breaks
 for i in [0, 2]:
-	for j in range(len(sections[i])):
-		print ' '.join(sections[i][j]), ' '.join(sections[i+1][j])
+    for j in range(len(sections[i])):
+        print ' '.join(sections[i][j]), ' '.join(sections[i+1][j])
